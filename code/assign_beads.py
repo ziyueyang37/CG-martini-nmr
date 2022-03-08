ae8521908131d4fad47d64d033d2153014417d06
ZiyueYang37 <zyang43@ur.rochester.edu>
2022-03-08 12:34:41 -0500

    add data

--- code/assign_beads.py
+++
@@ -1,8 +0,0 @@
-import MDAnalysis as mda
-
-u_itp = mda.Universe('../itpfiles/1A2P.itp', topology_format='ITP')
-u_pdb = mda.Universe('../mtn/1A2P_CG.pdb')
-print(u_itp.atoms.types)
-print(len(u_itp.atoms.types))
-print(u_pdb.atoms.types)
-print(len(u_pdb.atoms.types))
